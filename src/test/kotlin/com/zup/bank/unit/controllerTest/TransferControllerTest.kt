57977e6f3d89a25ed0b999b0043c0baa4e20a973
PedroMede <pedro_hsmedeiros@hotmail.com>
2020-01-28 18:43:53 -0300

    Unit test on controller transfer

--- src/test/kotlin/com/zup/bank/unit/controllerTest/TransferControllerTest.kt
+++ src/test/kotlin/com/zup/bank/unit/controllerTest/TransferControllerTest.kt
@@ -1,35 +1,66 @@
 package com.zup.bank.unit.controllerTest
 
+import com.zup.bank.controller.TransferController
+import com.zup.bank.dto.TransferDTO
+import com.zup.bank.dto.TransferDTOResponse
+import com.zup.bank.model.Account
+import com.zup.bank.model.Client
+import com.zup.bank.model.Transfer
+import com.zup.bank.service.ServiceTransfer
+import org.junit.Before
+
+import org.junit.Test
+import org.mockito.Mockito
+
+
 class TransferControllerTest {
 
-//    private val transferController : TransferController = TransferController(
-//        Mockito.mock(ServiceTransfer:: class.java),
-//        Mockito.mock(KafkaTemplate<String,Transfer>::class.java)
-//    )
-//
-//    lateinit var transferDTO : TransferDTO
-//    lateinit var accountOrigin: Account
-//    lateinit var accountDestiny: Account
-//    lateinit var client: Client
-//    lateinit var transfer: Transfer
-//
-//    @Test
-//    fun createTransfer(){
-//        client = Client(1,"Pedro","pedro@gmail.com","42511229846")
-//        accountDestiny = Account(2,"0001","19", client,100.00,true)
-//        accountOrigin = Account(1,"0001","18", client,100.00,true)
-//        transferDTO = TransferDTO("18","19",20.00)
-//        transfer = Transfer(1,accountOrigin,accountDestiny,20.00,null)
-//
-//        Mockito.`when`(transferController.transferServ.transfer(transferDTO)).thenReturn(transfer)
-//
-//        transferController.transfer(transferDTO)
-//
-//        Assert.assertEquals(HttpStatus.OK.value(), HttpStatus.OK.value())
-//        Mockito.verify(transferController.transferServ,Mockito.times(1)).transfer(transferDTO)
-//
-//    }
-//
+    private val transferController : TransferController = TransferController(
+        Mockito.mock(ServiceTransfer:: class.java)
+    )
+
+    lateinit var transferDTO : TransferDTO
+    lateinit var accountOrigin: Account
+    lateinit var accountDestiny: Account
+    lateinit var client: Client
+    lateinit var transfer: Transfer
+    lateinit var transferResponse : TransferDTOResponse
+
+    @Before
+    fun create(){
+        client = Client(1,"Pedro","pedro@gmail.com","42511229846")
+        accountDestiny = Account(2,"0001","19", client,100.00,true)
+        accountOrigin = Account(1,"0001","18", client,100.00,true)
+        transferDTO = TransferDTO("18","19",20.00)
+        transfer = Transfer(1,accountOrigin,accountDestiny,20.00)
+        transferResponse = TransferDTOResponse("18","19",20.00)
+    }
+
+    @Test
+    fun createTransfer(){
+
+        Mockito.`when`(transferController.transferServ.postInKafka(transferDTO)).thenReturn(transferResponse)
+
+        transferController.postKafka(transferDTO)
+
+
+        Mockito.verify(transferController.transferServ,Mockito.times(1)).postInKafka(transferDTO)
+
+    }
+
+
+    @Test
+    fun `function get by id`(){
+
+        Mockito.`when`(transferController.transferServ.getById(1)).thenReturn(transfer)
+
+        transferController.getById(1)
+
+
+        Mockito.verify(transferController.transferServ,Mockito.times(1)).getById(1)
+    }
+
+
 
 
 }
--- src/test/kotlin/com/zup/bank/unit/serviceTest/TransferTest.kt
+++ src/test/kotlin/com/zup/bank/unit/serviceTest/TransferTest.kt
@@ -2,6 +2,8 @@ package com.zup.bank.unit.serviceTest
 
 import com.zup.bank.dto.ObjectKafka
 import com.zup.bank.dto.TransferDTO
+import com.zup.bank.dto.TransferDTOResponse
+import com.zup.bank.enum.StatusTransfer
 import com.zup.bank.enum.TypeOperation
 import com.zup.bank.exception.customErrors.NotSufficientBalanceException
 import com.zup.bank.exception.customErrors.TransferToSameAccException
@@ -13,10 +15,16 @@ import com.zup.bank.repository.AccountRepository
 import com.zup.bank.repository.OperationsRepository
 import com.zup.bank.repository.TransferRepository
 import com.zup.bank.service.serviceImpl.TransferServImpl
+import org.apache.kafka.clients.consumer.MockConsumer
+import org.apache.kafka.clients.consumer.OffsetResetStrategy
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentCaptor
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.kafka.core.KafkaTemplate
 import java.util.*
 
@@ -41,9 +49,13 @@ class TransferTest {
     lateinit var operationOrigin: Operations
     lateinit var operationDestiny: Operations
     lateinit var transfer: Transfer
+    lateinit var transferResponse: Transfer
+    lateinit var trDTO:TransferDTO
+
 
     @Before
     fun create(){
+        trDTO = TransferDTO("18","19",20.0)
         transferDTO  =  ObjectKafka("18","19",20.00, 1L )
         clientOrigin = Client(1,"Pedro","pedro@gmail.com","42511229846")
         clientDestiny = Client(2,"Lucia","lucia@gmail.com","88804879653")
@@ -52,7 +64,9 @@ class TransferTest {
         accDestiny = Account(1,"0001","19", clientDestiny,100.00,true)
         operationOrigin = Operations(1,TypeOperation.TRANFER,50.00, Date(),accOrigin)
         operationDestiny = Operations(1,TypeOperation.TRANFER,50.00, Date(),accDestiny)
-        transfer = Transfer(1,accOrigin ,accDestiny,20.00)
+        transfer = Transfer(null,accOrigin ,accDestiny,20.00)
+        transferResponse = Transfer(1,accOrigin ,accDestiny,20.00)
+
     }
 
 
@@ -64,30 +78,6 @@ class TransferTest {
         servTransfer.existOrEqualsAcc(origin,dest)
     }
 
-    @Test
-    fun `method post in kafka working sucessfuly`(){
-
-        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("18")).thenReturn(accOrigin)
-        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("19")).thenReturn(accDestiny)
-        Mockito.`when`(servTransfer.accRepository.save(accOrigin)).thenReturn(accOrigin)
-        Mockito.`when`(servTransfer.accRepository.save(accOrigin)).thenReturn(accDestiny)
-        Mockito.`when`(servTransfer.opRepository.save(operationDestiny)).thenReturn(operationDestiny)
-        Mockito.`when`(servTransfer.opRepository.save(operationOrigin)).thenReturn(operationDestiny)
-        Mockito.`when`(servTransfer.transferRepo.save(Mockito.any(Transfer::class.java))).thenReturn(transfer)
-
-        servTransfer.transfer(this.transferDTO)
-
-        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
-            .findByNumberAcc("18")
-        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
-            .findByNumberAcc("19")
-        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
-            .save(accOrigin)
-        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
-            .save(accDestiny)
-        Mockito.verify(servTransfer.transferRepo,Mockito.times(1))
-            .save(Mockito.any(Transfer::class.java))
-    }
 
     @Test(expected = NotSufficientBalanceException::class)
     fun `method transfer not working because balance not sufficient`(){
@@ -103,4 +93,53 @@ class TransferTest {
         Mockito.verify(servTransfer.accRepository,Mockito.times(1))
             .findByNumberAcc("19")
     }
+
+
+
+    @Test(expected = EmptyResultDataAccessException::class)
+    fun `function post in kafka that not found account`(){
+        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("99")).thenThrow(EmptyResultDataAccessException::class.java)
+        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("88")).thenThrow(EmptyResultDataAccessException::class.java)
+
+        val tranferForPost = TransferDTO("99","88",20.0)
+
+        servTransfer.postInKafka(tranferForPost)
+
+    }
+
+
+    @Test
+    fun `function post in kafka sucess`(){
+        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("18"))
+            .thenReturn(accOrigin)
+        Mockito.`when`(servTransfer.accRepository.findByNumberAcc("19"))
+            .thenReturn(accDestiny)
+
+        val transferArgumentCaptor: ArgumentCaptor<Transfer> = ArgumentCaptor.forClass(Transfer::class.java)
+        Mockito.`when`(servTransfer.transferRepo.save(transferArgumentCaptor.capture())).thenReturn(transferResponse)
+
+        servTransfer.postInKafka(trDTO)
+
+        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
+            .findByNumberAcc("18")
+        Mockito.verify(servTransfer.accRepository,Mockito.times(1))
+            .findByNumberAcc("19")
+        Mockito.verify(servTransfer.transferRepo,Mockito.times(1))
+            .save(transferArgumentCaptor.capture())
+
+        Assert.assertThat(transferArgumentCaptor.value.id, CoreMatchers.nullValue());
+    }
+
+    @Test
+    fun `get by id working`(){
+        Mockito.`when`(servTransfer.transferRepo.findById(1)).thenReturn(Optional.of(transfer))
+
+        servTransfer.getById(1)
+
+        Mockito.verify(servTransfer.transferRepo,Mockito.times(1))
+            .findById(1)
+    }
+
+
+
 }
